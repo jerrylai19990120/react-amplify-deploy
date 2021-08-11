0f0fc2ae7c69138d028d706ebd291c32e895c964
jerry <jerrylai19990120@gmail.com>
2021-08-11 10:49:04 -0400

    bug fix embedding not loading

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,7 +5,8 @@ const Search = (props) => {
     return (
         <div style={{width: '100%', height: 1000}}>
             <FNavBar />
-            <iframe src="http://www.fangsso.com/" style={{width: '100%', height: '100%'}} title="embedded-site"></iframe>
+            <object type="text/html" data="http://www.fangsso.com/" style={{width: '100%', height: '100%'}}>
+            </object>
         </div>
     )
 }
