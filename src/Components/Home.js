cd19c98794668d6a91156faa05c7c02e037ed85b
jerrylai19990120 <jerrylai19990120@gmail.com>
2021-08-17 11:56:21 -0400

    home page animation update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
-import React, {useRef} from 'react';
-import { Carousel, Image} from 'react-bootstrap';
+import React, {useRef, useEffect} from 'react';
+import { Container, Image, Col, Row} from 'react-bootstrap';
 import bgImg1 from '../Assets/bgImg1.png'
 import bgImg2 from '../Assets/bgImg2.png'
 import bgImg3 from '../Assets/bgImg3.png'
@@ -13,6 +13,9 @@ import animationData2 from '../Assets/Lottie/search.json'
 import animationData3 from '../Assets/Lottie/tutorial.json'
 import animationData4 from '../Assets/Lottie/signup.json'
 import animationData5 from '../Assets/Lottie/applications.json'
+import animationData6 from '../Assets/Lottie/meeting.json'
+import { Button } from 'react-bootstrap';
+import { init } from 'ityped'
 
 const Home = (props) => {
 
@@ -22,70 +25,29 @@ const Home = (props) => {
     const lottieRefTutorial = useRef()
     const lottieRefApps = useRef()
 
+    useEffect(()=>{
+        const myElement = document.querySelector('#myElement')
+        init(myElement, { showCursor: false, strings: ['The fast and visual way to understand your users'], loop: true, typeSpeed: 130 })
+    }, [])
+
     return (
         <div>
-            <Carousel variant="light">
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100"
-                    src={bgImg1}
-                    alt="First"
-                    height={500}
-                    />
-                    <Carousel.Caption>
-                    <h5>房企通服务</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100"
-                    src={bgImg2}
-                    alt="Second"
-                    height={500}
-                    />
-                    <Carousel.Caption>
-                    <h5>房税搜</h5>
-                    <p>文案此处</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100"
-                    src={bgImg3}
-                    alt="Third"
-                    height={500}
-                    />
-                    <Carousel.Caption>
-                    <h5>在线培训</h5>
-                    <p>文案此处</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100"
-                    src={bgImg4}
-                    alt="Forth"
-                    height={500}
-                    />
-                    <Carousel.Caption>
-                    <h5>会员注册</h5>
-                    <p>文案此处</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100"
-                    src={bgImg5}
-                    alt="Fifth"
-                    height={500}
-                    />
-                    <Carousel.Caption>
-                    <h5>应用软件</h5>
-                    <p>文案此处</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            <Container>
+                <Row>
+                    <Col>
+                        <div id="myElement" style={{fontFamily: 'sans-serif', fontSize: 30, marginTop: 120, height: 80}}></div>
+                        <p style={{fontFamily:'sans-serif', marginTop: 30}}>Everything your team needs to uncover insights and make the right changes</p>
+                        <Button variant="primary" style={{backgroundColor: '#4476BD', borderRadius: 18, marginTop: 20}} href="/vipsignup">会员注册</Button>
+                        <div style={{marginTop: 30}}>
+                            <span style={{fontFamily:'sans-serif', color: 'gray', marginTop: 20, fontSize: 13}}>创建一个账户</span><br />
+                            <span style={{fontFamily:'sans-serif', color: 'gray', marginTop: 20, fontSize: 13}}>快捷，免费，便利</span>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Lottie animationData={animationData6} style={{width: 540, height: 540}}/>
+                    </Col>
+                </Row>
+            </Container>
             <div style={{width: "100%", height: 138}}>
                 <div style={{width: "100%", height: "100%", position: "absolute", marginTop: 35}}>
                     <h4>房企通</h4>
